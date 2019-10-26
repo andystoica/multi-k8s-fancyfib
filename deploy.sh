063f09e6fb213e1f0de0faedb27f7eb6ce626532
Andy Stoica <andy.stoica@me.com>
2019-10-26 12:10:31 +0100

    Troubleshoot Travis build #3

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,10 @@
 docker build -t andystoica/fancyfib-client -f ./client/Dockerfile ./client
+docker build -t andystoica/fancyfib-api -f ./api/Dockerfile ./api
+docker build -t andystoica/fancyfib-worker -f ./worker/Dockerfile ./worker
+
+docker push andystoica/fancyfib-client
+docker push andystoica/fancyfib-api
+docker push andystoica/fancyfib-worker
+
+kubectl apply -f k8s
+kubectl set image deployments/api-deployment api=andystoica/fancyfib-api
